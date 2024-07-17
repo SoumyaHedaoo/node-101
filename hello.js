5ffbadbbe477b6198987e3b94b9fc57fa439c6d6
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-07-17 14:20:00 +0530

    fs module function implemented

--- hello.js
+++ hello.js
@@ -1,10 +1,18 @@
 //const os=require("os");
 //console.log(os.cpus().length)
 // console.log("hellow World!!!")
+// 
+// const math=require("./math");
 
-const math=require("./math");
+// console.log(`addition is${math.add(2,7)}`);
+// console.log(`Subtraction is${math.sub(6,2)}`);
 
-console.log(`addition is${math.add(2,7)}`);
-console.log(`Subtraction is${math.sub(6,2)}`);
+const fs=require("fs");
+fs.writeFileSync("./test.txt" , "hii there!!");
 
+fs.writeFile("./test.txt" , "hiiii new async message" , ()=>{});
+
+fs.readFile("test.txt" , "utf-8" , (err , data)=>{
+    console.log(data)
+});
 
