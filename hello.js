1f8a8304a39264022f4660239c814ca1710a093d
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-07-18 15:14:49 +0530

    server log entry file system created

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@
 // console.log(`addition is${math.add(2,7)}`);
 // console.log(`Subtraction is${math.sub(6,2)}`);
 
-// const fs=require("fs");
+
 // fs.writeFileSync("./test.txt" , "hii there!!");
 
 // fs.writeFile("./test.txt" , "hiiii new async message" , ()=>{});
@@ -15,12 +15,18 @@
 // fs.readFile("test.txt" , "utf-8" , (err , data)=>{
 //     console.log(data)
 // }); 
-
+const fs=require("fs");
 const http=require("http");
 
 const myserver=http.createServer((req,res)=>{
-    console.log(req.headers);
-    res.end("hii i am server");
+    if(req.url==="/favicon.ico"){return; }
+    const x=new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+
+    fs.appendFile("log.txt" ,`${x} : user accessed: ${req.url}\n` , (err , data)=>{
+        res.end("hii the server is ready to use")
+        console.log("new acces granted")
+
+    });
 })
 
-myserver.listen(8000 , ()=>{console.log("server started")});
+myserver.listen(8000 , ()=>{console.log("server started")}); 
