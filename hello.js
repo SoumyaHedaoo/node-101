9da00cc5109f998341694c60e1f5d3bbe63cd575
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-07-18 14:10:08 +0530

    Server created using http module

--- hello.js
+++ hello.js
@@ -7,12 +7,20 @@
 // console.log(`addition is${math.add(2,7)}`);
 // console.log(`Subtraction is${math.sub(6,2)}`);
 
-const fs=require("fs");
-fs.writeFileSync("./test.txt" , "hii there!!");
+// const fs=require("fs");
+// fs.writeFileSync("./test.txt" , "hii there!!");
 
-fs.writeFile("./test.txt" , "hiiii new async message" , ()=>{});
+// fs.writeFile("./test.txt" , "hiiii new async message" , ()=>{});
 
-fs.readFile("test.txt" , "utf-8" , (err , data)=>{
-    console.log(data)
-});
+// fs.readFile("test.txt" , "utf-8" , (err , data)=>{
+//     console.log(data)
+// }); 
 
+const http=require("http");
+
+const myserver=http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.end("hii i am server");
+})
+
+myserver.listen(8000 , ()=>{console.log("server started")});
