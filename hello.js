ad526233bc8795466aa2fdb9039555423bf261f5
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-07-21 11:49:24 +0530

    Express implemented

--- hello.js
+++ hello.js
@@ -15,17 +15,51 @@
 // fs.readFile("test.txt" , "utf-8" , (err , data)=>{
 //     console.log(data)
 // }); 
-const fs=require("fs");
+// const fs=require("fs");
+// const url = require("url");
 const http=require("http");
+const express =require("express");
 
-const myserver=http.createServer((req,res)=>{
+const app = express();
+
+app.get("/" , (req , res)=>{
+    return res.end(`i guess your name is ${req.query.name} and your age is ${req.query.age} `);
+})
+
+app.get("/about" , (req , res)=>{
+    return res.end("i am developer my name is batman");
+})
+
+app.listen(8000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const myserver=http.createServer((req,res)=>{
    
     if(req.url==="/favicon.ico"){return; }
     
     const x=new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-
+    const myurl=url.parse(req.url , true);
+    console.log(myurl);
     fs.appendFile("log.txt" ,`${x} : user accessed: ${req.url}\n` , (err , data)=>{
-        switch(req.url){
+        switch(myurl.pathname){
             case '/' : res.end("nothing but this")
             break;
             case'/about' : res.end("yeah i am batman💀")
@@ -36,5 +70,5 @@ const myserver=http.createServer((req,res)=>{
         }
     });
 })
+*/
 
-myserver.listen(8000); 
